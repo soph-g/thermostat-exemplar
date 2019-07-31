3f74fc4a4c170df31dceb20913980f4ca9fb289e
Sophie Gill <sophie@makersacademy.com>
2019-07-31 10:28:19 +0100

    increasing temperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,15 +1,22 @@
 'use strict';
 
 describe('Thermostat', function() {
-  var thermostat
+  var thermostat;
 
   beforeEach(function() {
     thermostat = new Thermostat();
-  })
+  });
 
   describe('#getCurrentTemperature', function() {
     it('is 20 degrees by default', function() {
       expect(thermostat.getCurrentTemperature()).toEqual(20);
+    });
+  });
+
+  describe('#up', function() {
+    it('increments the temperature', function() {
+      thermostat.up();
+      expect(thermostat.getCurrentTemperature()).toEqual(21);
     });
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -7,3 +7,7 @@ function Thermostat() {
 Thermostat.prototype.getCurrentTemperature = function() {
   return this._temperature;
 }
+
+Thermostat.prototype.up = function() {
+  this._temperature += 1;
+}
