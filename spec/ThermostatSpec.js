c8e63b287a0fb3ee48846612aa59237d22216500
Sophie Gill <sophie@makersacademy.com>
2019-07-31 10:43:19 +0100

    adds minimum temperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -25,5 +25,12 @@ describe('Thermostat', function() {
       thermostat.down();
       expect(thermostat.getCurrentTemperature()).toEqual(19);
     });
+
+    it('has a minimum temperature of 10 degrees', function() {
+      for (var i = 0; i < 12; i++) {
+        thermostat.down();
+      }
+      expect(thermostat.getCurrentTemperature()).toEqual(10);
+    })
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,6 +1,7 @@
 'use strict';
 
 function Thermostat() {
+  this._MINIMUM_TEMPERATURE = 10;
   this._temperature = 20;
 }
 
@@ -12,6 +13,12 @@ Thermostat.prototype = {
     this._temperature += 1;
   },
   down: function() {
+    if (this._isMinimumTemperature()) {
+      return;
+    }
     this._temperature -= 1;
+  },
+  _isMinimumTemperature: function() {
+    return this._temperature == this._MINIMUM_TEMPERATURE;
   }
 };
