9b45527b4b4eca6c137c8b1347974286400d4e2f
Sophie Gill <sophie@makersacademy.com>
2019-07-31 16:58:58 +0100

    adds feature tests

---
+++ spec/features/power_saving_mode_spec.rb
@@ -0,0 +1,44 @@
+feature 'power saving mode' do
+  before do
+    visit('/')
+  end
+
+  it 'is on by default' do
+    expect(page.find('#power-saving-status')).to have_content 'on'
+  end
+
+  it 'can be switched off' do
+    page.find('#power-saving-off').click
+    expect(page.find('#power-saving-status')).to have_content 'off'
+  end
+
+  context 'power saving mode is on' do
+    it 'has a maximum temperature of 25' do
+      6.times do
+        page.find('#temperature-up').click
+      end
+      expect(page.find('#temperature')).to have_content 25
+    end
+
+    it 'resets the temperature if it is over 25' do
+      page.find('#power-saving-off').click
+      8.times do
+        page.find('#temperature-up').click
+      end
+      expect(page.find('#temperature')).to have_content 28
+      page.find('#power-saving-on').click
+      expect(page.find('#temperature')).to have_content 25
+    end
+  end
+
+  context 'power saving mode is off' do
+    it 'has a maximum temperature of 32' do
+      visit('/')
+      page.find('#power-saving-off').click
+      13.times do
+        page.find('#temperature-up').click
+      end
+      expect(page.find('#temperature')).to have_content 32
+    end
+  end
+end
---
+++ spec/features/temperature_spec.rb
@@ -0,0 +1,27 @@
+feature 'viewing the temperature' do
+  before do
+    visit('/')
+  end
+  it 'is 20 by default' do
+    expect(page.find('#temperature')).to have_content '20'
+  end
+
+  it 'can be increased' do
+    page.find('#temperature-up').click
+    expect(page.find('#temperature')).to have_content '21'
+    expect(page).not_to have_content '20'
+  end
+
+  it 'can be decreased' do
+    page.find('#temperature-down').click
+    expect(page.find('#temperature')).to have_content '19'
+    expect(page).not_to have_content '20'
+  end
+
+  it 'can be reset' do
+    page.find('#temperature-down').click
+    expect(page.find('#temperature')).to have_content '19'
+    page.find('#temperature-reset').click
+    expect(page.find('#temperature')).to have_content '20'
+  end
+end
